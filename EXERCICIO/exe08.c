0f62de8d3367ed23024119e9a0c488e1a7203c98
leticiakaren2015 <leticiakaren2015@gmail.com>
2026-06-16 13:31:06 +0100

    Usado While

---
+++ EXERCICIO/exe08.c
@@ -0,0 +1,22 @@
+// Pede ao utilizador um número positivo.Enquanto o o número for negativo ou zero,
+// o programa deve voltar a pedir o número.
+
+#include <stdio.h>
+
+int main(){
+
+    int num;
+
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    while (num < 1)
+    {
+        printf("Digite um número: ");
+        scanf("%d", &num);
+    }
+
+    
+    printf("Fim do Programa");
+    return 0;
+}
