db1560e7aec0c2bec46dea425b763e63f0cff94e
leticiakaren2015 <leticiakaren2015@gmail.com>
2026-07-23 13:39:17 +0100

    Vetor

--- EXERCICIO/Estudo_em_C/005.c
+++ EXERCICIO/Estudo_em_C/005.c
@@ -12,10 +12,48 @@ int main() {
         {
             printf("Enter a number %d: ", i + 1);
             scanf("%f", &notas[i]);
+            if(notas[i] < 0 || notas[i] > 20){
+                printf("\n____INVALID NUMBER___\n\n");
+            }
+
+        
         } while (!(notas[i] >= 0 && notas[i] <= 20));
         
     }
+
+    // All the notes entered
+    printf("\n");
+    for(int i = 0; i < TAM; i++){
+        printf("%.2f\n", notas[i]);
+    }
+
+    // Class average
+    float media; 
+    float soma = 0;
+
+    for(int i = 0; i < TAM; i++){
+        soma+=notas[i];
+    }
+    media = soma / TAM;
+    printf("\nA média é %.2f\n", media);
     
+    // Highest score
+
+    float maior = notas[0];
+    float menor = notas[0];
     
+    for(int i = 0; i < TAM; i++){
+        if(notas[i] > maior){
+            maior = notas[i];
+        }
+        if(notas[i] < menor){
+            menor = notas[i];
+        }
+    }
+
+    printf("A maior nota é %.2f\n", maior);
+    printf("A menor nota é %.2f\n", menor);
+
+
     return 0;
 }
